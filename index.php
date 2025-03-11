a3e62af4822fef2ef8b5000669034d6f0c3ae7c6
giuseppecappadonna <giuseppecappadonna22@gmail.com>
2025-03-11 22:36:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prodotti</title>
+</head>
+<body>
+    <ul id="navbar">
+        <li><a href="#">Prodotti</a></li>
+        <li><a href="preferiti.php">Preferiti</a></li>
+    </ul>
+    <header>
+
+    </header>
+    <section class="max-w pdg">
+        <div class="flex-cont">
+            <div class="card">
+                
+                <h3>Lorem 1</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+            
+        </div>
+    </section>
+    
+</body>
+</html>
