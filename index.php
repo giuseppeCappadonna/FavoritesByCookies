aad43caeed4827070070c9fb15a71ff3b55ba5e6
giuseppecappadonna <giuseppecappadonna22@gmail.com>
2025-03-11 23:30:51 +0100

    front end

--- index.php
+++ index.php
@@ -4,27 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Prodotti</title>
+    <link rel="stylesheet" href="/res/css/home.css">
 </head>
 <body>
-    <ul id="navbar">
-        <li><a href="#">Prodotti</a></li>
-        <li><a href="preferiti.php">Preferiti</a></li>
-    </ul>
     <header>
-
+        <a href="#" class="logo">Logo</a>
+        <div class="mean-toggle"></div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="preferiti.php">Preferiti</a></li>
+            </ul>
+        </nav>
+        <div class="clear"></div>
     </header>
-    <section class="max-w pdg">
+
+    <section id="items" class="max-w pdg">
         <div class="flex-cont">
-            <div class="card">
-                
-                <h3>Lorem 1</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
+            <div class="card" id="001">
+                <div class="img-container">
+                    <img class="phone"  src="https://ariskost.github.io/images-for-test-projects/galaxy-s23-ultra.png" alt="Samsung Galaxy S23 Ultra">
+                </div>
+                <div class="container">
+                    <h1>001 - Lorem ipsum</h1>
+                    <h3 class="price">1,449.<small>00</small>&euro;</h3>
+                    <a href="#" class="favor">Add Favorites</a>
+                </div>
             </div>
-            
+
+            <div class="card" id="002">
+                <div class="img-container">
+                    <img class="phone"  src="https://ariskost.github.io/images-for-test-projects/galaxy-s23-ultra.png" alt="Samsung Galaxy S23 Ultra">
+                </div>
+                <div class="container">
+                    <h1>002 - Lorem ipsum</h1>
+                    <h3 class="price">1,449.<small>00</small>&euro;</h3>
+                    <a href="#" class="favor">Add Favorites</a>
+                </div>
+            </div>
+
+            <div class="card added" id="003">
+                <div class="img-container">
+                    <img class="phone"  src="https://ariskost.github.io/images-for-test-projects/galaxy-s23-ultra.png" alt="Samsung Galaxy S23 Ultra">
+                </div>
+                <div class="container">
+                    <h1>003 - Lorem ipsum</h1>
+                    <h3 class="price">1,449.<small>00</small>&euro;</h3>
+                    <a href="#" class="favor">Add Favorites</a>
+                </div>
+            </div>
         </div>
     </section>
+
     
 </body>
 </html>
